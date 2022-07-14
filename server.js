8990fb30632add2e9681ea8904dbc6c108003ed9
Aly Ibrahim <aly.ibrahim@gmail.com>
2022-07-14 14:35:14 +0000

    Initial COmmit

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var tenant = require('./tenant-api.js');
+
+app.use(bodyParser.json());
+app.use('/tenant', tenant)
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+var server = app.listen(8080, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
+
+module.exports = app
---
+++ tenant-api.js
@@ -0,0 +1,121 @@
+//import AWS from 'aws-sdk'
+// import { nanoid } from 'nanoid';
+
+var AWS = require('aws-sdk');
+// var { nanoid } = require("nanoid");
+var express = require('express')
+var router = express.Router();
+
+const ddb = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' });
+
+const table = 'tenants'
+
+// Home page route.
+router.get('/', async function (req, res) {
+  console.log("Get tenants")
+  let data = await getAll("tenants")
+  res.send(data);
+//  res.send('Wiki home page');
+})
+
+router.post('/', async function (req, res) {
+//  console.log(req)
+  let data = req.body;
+//  console.log(JSON.parse(data))
+  console.log(data)
+  let resp = await insertTenant(data.name, data.tier, data.base_domain);
+  console.log(resp)
+  res.send(resp)
+})
+
+router.put('/', async function (req, res) {
+  let data = req.body;
+  console.log(data)
+  let resp = await updateTenant(data.name, data.tier, data.base_domain);
+  console.log(resp)
+  res.send(resp)
+
+})
+
+router.delete('/', async function (req, res) {
+  let data = req.body;
+  console.log(data)
+  let resp = await deleteTenant(data.name);
+  console.log(resp)
+  res.send(resp)
+
+})
+
+
+
+const insertTenant = async (name, tier, base_domain) => {
+  const params = {
+    TableName: table,
+    Item: {
+      tenant: name,
+      user: name,
+      tier: tier,
+      dbname: name,
+      base_domain: base_domain
+    },
+  };
+  try {
+    const data = await ddb.put(params).promise();
+    console.log('Tenant inserted', data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+
+const getAll = async (table) => {
+  const params = {
+    TableName: table,
+  };
+  try {
+    const data = await ddb.scan(params).promise();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const deleteTenant = async (name) => {
+  const params = {
+    TableName: table,
+    Key: {
+      tenant: name,
+    }
+  };
+  try {
+    const data = await ddb.delete(params).promise();
+    console.log('Tenant deleted', data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const updateTenant = async (name, tier, base_domain) => {
+  const params = {
+    TableName: table,
+    Item: {
+      tenant: name,
+      user: name,
+      tier: tier,
+      dbname: name,
+      base_domain: base_domain
+    },
+  };
+  try {
+    const data = await ddb.put(params).promise();
+    console.log('Tenant updated', data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+
+
+
+module.exports = router;
